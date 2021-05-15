7fcff0fa86d9a162f41f4a72596a1ec6de12a721
Islam <islamsalmanov888@gmail.com>
2021-05-15 15:12:45 +0300

    Add javascript

---
+++ javascripts/app.js
@@ -0,0 +1,21 @@
+var main=function(){
+	"use strict";
+	$(".tabs a:nth-child(1)").on("click",function(){
+		$(".tabs span").removeClass("active");
+		$(".tabs a:nth-child(1) span").addClass("active");
+		$("main.content").empty();
+		return false;
+	})
+	$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(2)").on("click", function () {
+	$(".tabs span").removeClass("active");
+	$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(2) span").addClass("active");
+	$("main .content").empty();
+	return false;
+	});
+	$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(3)").on("click", function () {
+	$(".tabs span").removeClass("active");
+	$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(3) span").addClass("active");
+	$("main .content").empty();
+	return false;
+	});
+}
