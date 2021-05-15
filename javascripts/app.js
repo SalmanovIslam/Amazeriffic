0014143afe5ccd11b0ff6c854a749432b31cdce5
Islam <islamsalmanov888@gmail.com>
2021-05-15 15:21:49 +0300

    improvment with help of function javascript2

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,20 +1,20 @@
 var main = function () {
 "use strict";
 var makeTabActive = function (tabNumber) {
-var tabSelector = ".tabs a:nth-код для этого элемента может выглядеть вот так:child(" + tabNumber + ") span";
+var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
 $(".tabs span").removeClass("active");
 $(tabSelector).addClass("active");
 };
-$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(1)").on("click", function () {
+$(".tabs a:nth-child(1)").on("click", function () {
 5
 makeTabActive(1);
 return false;
 });
-$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(2)").on("click", function () {
+$(".tabs a:nth-child(2)").on("click", function () {
 makeTabActive(2);
 return false;
 });
-$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(3)").on("click", function () {
+$(".tabs a:nth-child(3)").on("click", function () {
 makeTabActive(3);
 return false;
 });
