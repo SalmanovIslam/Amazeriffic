208ecc1d25e2267b9dea5b97c92847432a47fdca
Islam <islamsalmanov888@gmail.com>
2021-05-16 13:30:09 +0300

    added tage in code

--- javascripts/app.js
+++ javascripts/app.js
@@ -22,8 +22,47 @@ $(".tabs a span").toArray().forEach(function (element) {
 			toDos.forEach(function (todo) { 
 				$(".content").append($("<li>").text(todo));
 			});
-		} 
-		else if ($element.parent().is(":nth-child(3)")) { 
+		} else if($element.parent().is(":nth-child(3)")){
+			//ЭТО КОД ДЛЯ ВКЛАДКИ ТЕГИ
+			console.log("щелчок на вкладку Теги");
+			var organizedByTag=[
+			{
+				"name": "покупки",
+				"toDos": ["Купить продукты"]
+			},
+			{
+				"name": "рутина",
+				"toDos": ["Купить продукты", "Вывести Грейси на прогулку в парк"]
+			},
+			{
+				"name": "писательство",
+				"toDos": ["Сделать несколько новых задач", "Закончить писать книгу"]
+			},
+			{
+				"name": "работа",
+				"toDos": ["Сделать несколько новых задач", "Подготовиться к лекции в понедельник","Ответить на электронные письма", "Закончить писать книгу"]
+			},
+			{
+				"name": " преподавание",
+				"toDos": ["Подготовиться к лекции в понедельник"]
+			},
+			{
+				"name": "питомцы",
+				"toDos": ["Вывести Грейси на прогулку в парк "]
+			}
+			];
+			organizedByTag.forEach(function (tag) {
+			var $tagName = $("<h3>").text(tag.name),
+			$content = $("<ul>");
+			tag.toDos.forEach(function (description) {
+			var $li = $("<li>").text(description);
+			$content.append($li);
+			});
+			$("main .content").append($tagName);
+			$("main .content").append($content);
+			});
+		}
+		else if ($element.parent().is(":nth-child(4)")) { 
 			$(".content").append(
 				'<input type="text" class="inp">'+
 				'<button class="btn">Добавить</button>'
