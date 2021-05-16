5d43b43f76195ec9dfa27958b45c755eec90d863
Islam <islamsalmanov888@gmail.com>
2021-05-16 22:25:59 +0300

    Add DB

--- Express/server.js
+++ Express/server.js
@@ -1,41 +1,44 @@
 var express = require("express"),
-http = require("http"),
-app = express();
-toDos =[
-{
-"description" : "Купить продукты",
-"tags" : [ "шопинг", "рутина" ]
-},
-{
-"description" : "Сделать несколько новых задач",
-"tags" : [ "писательство", "работа" ]
-},
-{
-"description" : "Подготовиться к лекции в понедельник", 
-"tags" : [ "работа", "преподавание" ]
-},
-{
-"description" : "Ответить на электронные письма", 
-"tags" : [ "работа" ]
-},
-{
-"description" : "Вывести Грейси на прогулку в парк", 
-"tags" : [ "рутина", "питомцы" ]
-},
-{
-"description" : "Закончить писать книгу", 
-"tags" : [ "писательство", "работа" ]
-}
-]
+	http = require("http"),
+	mongoose = require("mongoose"),
+	app = express();
 app.use(express.static("C:/Users/Islam/Desktop/Веб прг/Практика/Amazeriffic/client"));
+var ToDoSchema = mongoose.Schema({
+	description: String,
+	tags: [ String ]
+});
+var ToDo = mongoose.model("ToDo", ToDoSchema);
 http.createServer(app).listen(3000);
-app.get("/todos.json",function(req,res){
-	res.json(toDos);
+app.get("/todos.json", function (req, res) {
+	ToDo.find({}, function (err, toDos) {
+		res.json(toDos);
+	});
 });
 app.use(express.urlencoded({ extended: true }));
+mongoose.connect('mongodb://localhost/amazeriffic', {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true 
+	}).then(res => {
+		console.log("DB Connected!")
+	}).catch(err => {
+		console.log(Error, err.message);
+	});
 app.post("/todos", function (req, res) {
-	var newToDo = req.body;
-	console.log(newToDo);
-	toDos.push(newToDo);
-	res.json({"message":"Вы размещаетеся на сервере!"});
+	console.log(req.body);
+	var newToDo = new ToDo({"description":req.body.description,
+		"tags":req.body.tags});
+	newToDo.save(function (err, result) {
+		if (err !== null) {
+			console.log(err);
+			res.send("ERROR");
+		} else {
+			ToDo.find({}, function (err, result) {
+				if (err !== null) {
+					res.send("ERROR");
+				}
+				res.json(result);
+			});
+		}
+	});
 });
