d8186325c5f35b81d6fcf34be56ad00c65f267a6
Islam <islamsalmanov888@gmail.com>
2021-05-25 13:36:54 +0300

    Change server.js

--- Express/server.js
+++ Express/server.js
@@ -1,52 +1,42 @@
 var express = require("express"),
 	http = require("http"),
 	mongoose = require("mongoose"),
-	var UserSchema = mongoose.Schema({
-		user:String,
-		id:String,
-	});
-	var User = mongoose.model("User",UserSchema);
-	module.export = User;
+	ToDosController = require("./controllers/todos_controller.js"),
+	UsersController = require("./controllers/users_controller.js"),
+	app = express(); 
 
-
-	app = express();
-app.use(express.static("C:/Users/Islam/Desktop/Веб прг/Практика/Amazeriffic/client"));
-var ToDoSchema = mongoose.Schema({
-	description: String,
-	tags: [ String ]
-});
-var ToDo = mongoose.model("ToDo", ToDoSchema);
 http.createServer(app).listen(3000);
-app.get("/todos.json", function (req, res) {
-	ToDo.find({}, function (err, toDos) {
-		res.json(toDos);
-	});
-});
+
+app.use('/',express.static("C:/Users/Islam/Desktop/Веб прг/Практика/Amazeriffic/client"));
+app.use('/user/:username',express.static("C:/Users/Islam/Desktop/Веб прг/Практика/Amazeriffic/client"));
+
+// командуем Express принять поступающие объекты JSON
 app.use(express.urlencoded({ extended: true }));
+
+// подключаемся к хранилищу данных Amazeriffic в Mongo
 mongoose.connect('mongodb://localhost/amazeriffic', {
 		useNewUrlParser: true,
 		useCreateIndex: true,
 		useUnifiedTopology: true 
-	}).then(res => {
-		console.log("DB Connected!")
-	}).catch(err => {
-		console.log(Error, err.message);
-	});
-app.post("/todos", function (req, res) {
-	console.log(req.body);
-	var newToDo = new ToDo({"description":req.body.description,
-		"tags":req.body.tags});
-	newToDo.save(function (err, result) {
-		if (err !== null) {
-			console.log(err);
-			res.send("ERROR");
-		} else {
-			ToDo.find({}, function (err, result) {
-				if (err !== null) {
-					res.send("ERROR");
-				}
-				res.json(result);
-			});
-		}
-	});
+}).then(res => {
+	console.log("DB Connected!")
+}).catch(err => {
+	console.log(Error, err.message);
 });
+
+app.get("/todos.json", ToDosController.index);
+app.get("/todos/:id", ToDosController.show); 
+app.post("/todos", ToDosController.create);
+app.put("/todos/:id", ToDosController.update);
+app.delete("/todos/:id", ToDosController.destroy);
+
+app.get("/users/:username/todos.json", ToDosController.index);
+app.post("/users/:username/todos", ToDosController.create);
+app.put("/users/:username/todos/:id", ToDosController.update);
+app.delete("/users/:username/todos/:id", ToDosController.destroy);
+
+app.get("/users.json", UsersController.index); 
+app.post("/users", UsersController.create); 
+app.get("/users/:username", UsersController.show);
+app.put("/users/:username", UsersController.update);
+app.delete("/users/:username", UsersController.destroy); 
