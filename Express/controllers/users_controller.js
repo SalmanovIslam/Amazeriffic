20d6848555cc237987c3d598386c5c4b23cc91b8
Islam <islamsalmanov888@gmail.com>
2021-05-28 14:41:02 +0300

    Bug fixes and configuration

--- Express/controllers/users_controller.js
+++ Express/controllers/users_controller.js
@@ -1,117 +1,113 @@
-var ToDo = require("../models/todo.js"),
-	User = require("../models/user.js"),
-	ToDosController = {};
-
-ToDosController.index = function (req, res) { 
-	console.log("Вызван ToDosController.index");
-	var username = req.params.username || null,
-		respondWithToDos;
-	respondWithToDos = function (query) { 
-		ToDo.find(query, function (err, toDos) {
-			if (err !== null) {
-				res.json(500, err);
-			} else {
-				res.status(200).json(toDos);
-			}
-		});
-	};
-	if (username !== null) {
-		console.log("Поиск пользователя: "+username);
-		User.find({"username": username}, function (err, result) {
-			if (err !== null) {
-				res.json(500, err);
-			} else if (result.length === 0) {
-				res.status(404).json({"result_length": 0});
-			} else {
-				respondWithToDos({"owner": result[0]._id});
-			}
-		});
-	} else {
-		respondWithToDos({});
-	}
+var ToDo = require("C:/Users/Islam/Desktop/Веб прг/Практика/Amazeriffic/Express/models/todo.js"),
+	User = require("C:/Users/Islam/Desktop/Веб прг/Практика/Amazeriffic/Express/models/user.js"),
+	UsersController = {},
+	mongoose = require("mongoose");
+ 
+UsersController.index = function(req, res) {
+	console.log('Вызвано действие: UsersController.index');
+	User.find(function (err, users) {
+  		if (err !== null) {
+			res.json(500, err);
+  		} else {
+  			res.status(200).json(users);
+  		}
+  	});
 };
 
-ToDosController.create = function (req, res) {
-	var username = req.params.username || null;
-	var newToDo = new ToDo({
-		"description": req.body.description,
-		"tags": req.body.tags
+// Отобразить пользователя 
+UsersController.show = function(req, res) {
+	console.log('Вызвано действие: отобразить пользователя');
+	User.find({'username': req.params.username}, function(err, result) {
+	if (err) {
+		console.log(err);
+	} else if (result.length !== 0) {
+		res.sendfile('C:/Users/Islam/Desktop/Веб прг/Практика/Amazeriffic/client/list.html');
+	} else {
+	  res.send(404);
+	}
 	});
+};
 
-	console.log("username: " + username);
+// Создать нового пользователя 
+UsersController.create = function(req, res) {
+	console.log('Вызвано действие: создать пользователя');
+	var username = req.body.username; 
+	// console.log(username);
+	User.find({"username": username}, function (err, result) {
+	    if (err) {
+	        console.log(err);
+	        res.send(500, err);
+	    } else if (result.length !== 0) {
+	        res.status(501).send("Пользователь уже существует");
+	        console.log(err);   
+	        console.log("Пользователь уже существует"); 
+	    } else {
+	        var newUser = new User({
+	            "username": username
+	        });
+	        newUser.save(function(err, result) {
+	            console.log(err); 
+	            if (err !== null) {
+	                res.json(500, err); 
+	            } else {
+	                res.json(200, result);
+	                console.log(result); 
+	            }
+	        });
+	    }
+	}); 
+};
 
+// Обновить существующего пользователя 
+UsersController.update = function (req, res) { 
+	console.log("Вызвано действие: обновить пользователя");
+	var username = req.params.username;
+	console.log("Старое имя пользователя: " + username);
+	var newUsername = {$set: {username: req.body.username}};
+	console.log("Новое имя пользователя: " + req.body.username);
+	User.updateOne({"username": username}, newUsername, function (err,user) {
+		if (err !== null) {
+			res.status(500).json(err);
+		} else {
+			if (user.n === 1 && user.nModified === 1 && user.ok === 1) {
+				console.log('Изменен');
+				res.status(200).json(user);
+			} else {
+				res.status(404);
+			}
+		}
+	});
+};
+
+// Удалить существующего пользователя 
+UsersController.destroy = function (req, res) { 
+	console.log("Вызвано действие: удалить пользователя");
+	var username = req.params.username;
 	User.find({"username": username}, function (err, result) {
 		if (err) {
-			res.send(500);
-		} else {
-			if (result.length === 0) {
-				newToDo.owner = null;
-			} else {
-				newToDo.owner = result[0]._id;
-			}
-			newToDo.save(function (err, result) {
-				console.log(result);
-				if (err !== null) {
-					// элемент не был сохранен!
-					console.log(err);
-					res.json(500, err);
-				} else {
-					res.status(200).json(result);
-				}
-			});
-		}
-	});
-};
-
-ToDosController.show = function (req, res) {
-	// это ID, который мы отправляем через URL
-	var id = req.params.id;
-	// находим элемент списка задач с соответствующим ID 
-	ToDo.find({"_id":id}, function (err, todo) {
-		if (err !== null) {
-			// возвращаем внутреннюю серверную ошибку 
-			res.status(500).json(err);
-		} else {
-			if (todo.length > 0) {
-				// возвращаем успех!
-				res.status(200).json(todo[0]);
-			} else {
-				// мы не нашли элемент списка задач с этим ID! 
-				res.send(404);
-			}
-		}
-	});
-};
-
-ToDosController.destroy = function (req, res) {
-	var id = req.params.id;
-	ToDo.deleteOne({"_id": id}, function (err, todo) {
-		if (err !== null) {
-			res.status(500).json(err);
-		} else {
-			if (todo.n === 1 && todo.ok === 1 && todo.deletedCount === 1) {
-				res.status(200).json(todo);
-			} else {
-				res.status(404).json({"status": 404});
-			}
-		}
+            console.log(err);
+            res.send(500, err);
+        } else if (result.length !== 0) {
+        	console.log("Удаляем все todo с 'owner': " + result[0]._id);
+        	ToDo.deleteMany({"owner": result[0]._id}, function (err, todo) {
+		        console.log("Удаляем пользователя");
+				User.deleteOne({"username": username}, function (err, user) {
+					if (err !== null) {
+						res.status(500).json(err);
+					} else {
+						if (user.n === 1 && user.ok === 1 && user.deletedCount === 1) {
+							res.status(200).json(user);
+						} else {
+							res.status(404).json({"status": 404});
+						}
+					}
+				});
+        	});
+        } else {
+            res.status(404).send("Пользователь не существует");
+            console.log(err);   
+        }
 	});
 }
 
-ToDosController.update = function (req, res) {
-	var id = req.params.id;
-	var newDescription = {$set: {description: req.body.description}};
-	ToDo.updateOne({"_id": id}, newDescription, function (err,todo) {
-		if (err !== null) {
-			res.status(500).json(err);
-		} else {
-			if (todo.n === 1 && todo.nModified === 1 && todo.ok === 1) {
-				res.status(200).json(todo);
-			} else {
-				res.status(404).json({"status": 404});
-			}
-		}
-	});
-}
-
-module.exports = ToDosController;
+module.exports = UsersController;
--- Express/models/todo.js
+++ Express/models/todo.js
@@ -1,7 +1,6 @@
 var mongoose = require("mongoose"),
 	ObjectId = mongoose.Schema.Types.ObjectId;
 	
-// Это модель mongoose для списка задач
 var ToDoSchema = mongoose.Schema({
 	description: String,
 	tags: [ String ],
